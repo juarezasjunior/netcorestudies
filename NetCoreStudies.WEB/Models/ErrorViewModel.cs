b4492ac1e4e8de45391529793ded7aa4abf5206d
Juarez Júnior <juarez.a.s.junior@gmail.com>
2020-06-04 19:59:19 -0300

    Check-in inicial do módulo NetCoreStudies

---
+++ NetCoreStudies.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreStudies.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
